3ab8f3864c8cb036aeaae20c232dd1b30e53e98e
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-03-13 21:55:19 +0100

    changes on main page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import s from './Home.module.css';
+
 import icons from '/public/vite.svg'
 const Home = () => {
   return (
@@ -8,59 +9,69 @@ const Home = () => {
     <header className={s.header}>
     <a href="/"><p className={s.logo}>SENDYKUS</p></a>
   </header>
-  <div className={s.cont_page}>
   <main>
-    <section className={s.section_home}>
-    <div className={s.icons_f}>
-<svg>
-  <use href={`${icons}#icon-tg`} width="108" height="98"></use>
-  </svg>
+  <div className={s.cont_page}>
 
-  <svg>
-  <use href={`${icons}#icon-msg`} width="118" height="108"></use>
-  </svg>
+<section className={s.section_home}>
+<div className={s.icons_f}>
+  
+  <div>
+  <svg width="108" height="98">
+<use href={`${icons}#icon-tg`}></use>
+</svg>
+  </div>
+<div><svg width="118" height="108">
+<use href={`${icons}#icon-msg`}></use>
+</svg></div>
+
+
 </div>
 <div className={s.cont_icons_txt}>
 <div className={s.cont_txt}>
-    <p className={s.hero_title}>Connect instantly. Chat freely. Stay close
-    </p>
-    <p className={s.hero_txt}>no matter the distance!</p>
-    <button className={s.btn_get_started}>Get started</button>
-    </div>
+<p className={s.hero_title}>Connect instantly. Chat freely. Stay close
+</p>
+<p className={s.hero_txt}>no matter the distance!</p>
+<button className={s.btn_get_started}>Get started</button>
+</div>
 <div className={s.cont_icons_adaptive}>
 {/* <svg>
-  <use href={`${icons}#icon-tg`} width="80" height="70"></use>
-  </svg>
+<use href={`${icons}#icon-tg`} width="80" height="70"></use>
+</svg>
 
-  <svg>
-  <use href={`${icons}#icon-msg`} width="80" height="70"></use>
-  </svg>
+<svg>
+<use href={`${icons}#icon-msg`} width="80" height="70"></use>
+</svg>
 
 
-    <svg>
-  <use href={`${icons}#icon-phone`} width="80" height="77"></use>
-  </svg>
+<svg>
+<use href={`${icons}#icon-phone`} width="80" height="77"></use>
+</svg>
 
-  <svg>
-  <use href={`${icons}#icon-clouds`} width="80" height="70"></use>
-  </svg> */}
+<svg>
+<use href={`${icons}#icon-clouds`} width="80" height="70"></use>
+</svg> */}
 </div>
 </div>
 
-    <div className={s.icons_s}>
-    <svg>
-  <use href={`${icons}#icon-phone`} width="112" height="87"></use>
-  </svg>
+<div className={s.icons_s}>
 
-  <svg>
-  <use href={`${icons}#icon-clouds`} width="113" height="90"></use>
-  </svg>
-    </div>
-    </section>
+  
+<svg width="120" height="97">
+<use href={`${icons}#icon-phone`} ></use>
+</svg>
+
+<svg width="123" height="97">
+<use href={`${icons}#icon-clouds`} ></use>
+</svg>
+</div>
+</section>
+
+
+</div>
+
 
   </main>
-    </div>
-
+  
   </div>
 
 )
