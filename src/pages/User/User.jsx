b8711f400a75dc1bcf1248b0b14bd5e373fe314b
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-03-25 22:05:50 +0100

    changes

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -2,9 +2,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { isLoggedIn } from "../../redux/auth/selectors.js";
 import { logOutThunk } from "../../redux/auth/operations.js";
 import { Navigate } from "react-router-dom";
+import ModalProfile from "../../components/ModalProfile/ModalProfile.jsx";
+import { useState } from "react";
 
 
 const User = () => {
+      const [open,setOpen]=useState(false);
+  
+      const handleOpenModal=()=>{
+          setOpen(true);
+      }
   const dispatch=useDispatch();
     const isLoggedInSel=useSelector(isLoggedIn);
 if (!isLoggedInSel){
@@ -16,7 +23,9 @@ return <Navigate to='/'></Navigate>
     {isLoggedInSel && (
       <div>
       <p>Hello, new user!</p>
+      <button onClick={handleOpenModal}>Profile</button>
       <button onClick={()=>dispatch(logOutThunk())}>Log out</button>
+      {open && <ModalProfile handleClose={() => setOpen(false)} />}
       </div>
     )}
     </>
