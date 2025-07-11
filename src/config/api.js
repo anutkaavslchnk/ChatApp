6bd663c20ff472e9282616cf5714e453fa458d53
Anna Vasylchenko <anutochkaaaaa14@gmail.com>
2025-07-11 18:46:00 +0200

    changes

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://chatapp-bd.onrender.com/",
+  baseURL: "https://chatapp-bd.onrender.com",
   withCredentials: true,
 });
 
